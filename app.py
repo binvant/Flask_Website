502e566f873a3e1cc67cca8bcb4a1eab93b35cf5
Binvant Bajwa <63010614+binvant@users.noreply.github.com>
2020-04-23 12:57:01 +0530

    First commit
    This is the code for a small website that i have developed using Python, Flask, PostgreSQL and SQLAlchemy. It has added functionalities like a login page, Reset password page, register page etc.

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/flasksql'
+app.config['SECRET_KEY'] = 'super-secret'
+app.config['SECURITY_REGISTERABLE']= True
+db = SQLAlchemy(app)
+
+roles_users = db.Table('roles_users',
+    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+
+@app.route('/')
+def index():
+ return render_template('index.html')
+
+
+@app.route('/profile/<email>')
+@login_required
+def profile(email):
+ user=User.query.filter_by(email=email).first()
+ return render_template('profile.html', user=user)
+
+
+
+@app.route('/post_user', methods=['POST'])
+def post_user():
+    user=User(request.form['username'], request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+   
+   
+if __name__=="__main__":
+    app.run(debug=True)
+     
